93673028e9184a09820f43f4a297f1b5dc98d048
opticyclic <opticyclic@gmail.com>
2017-01-29 14:30:59 -0500

    Rename device_NX529J.mk to omni_NX529J.mk as the build system maps the name in vendorsetup.sh to the mk file name

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_NX529J.mk
+    $(LOCAL_DIR)/omni_NX529J.mk
--- full_NX529J.mk
+++ full_NX529J.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := NX529J
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/nubia/NX529J/device_NX529J.mk)
+$(call inherit-product, device/nubia/NX529J/omni_NX529J.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := NX529J

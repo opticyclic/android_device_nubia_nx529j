8711bc3b4457170c9d2c2143d1366df05a870f68
opticyclic <opticyclic@gmail.com>
2017-01-29 22:50:23 -0500

    Group Kernel flags together in alphabetical order

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,10 +15,12 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := NX529J
 
-BOARD_MKBOOTIMG_ARGS :=  --ramdisk_offset 0x02000000 --tags_offset 0x00000100 --dt device/nubia/NX529J/dt.img
-BOARD_KERNEL_CMDLINE := console=null androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
+# Kernel
 BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS :=  --ramdisk_offset 0x02000000 --tags_offset 0x00000100 --dt device/nubia/NX529J/dt.img
+TARGET_PREBUILT_KERNEL := device/nubia/NX529J/kernel
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 41943040
@@ -29,8 +31,6 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/nubia/NX529J/twrp.fstab
-
-TARGET_PREBUILT_KERNEL := device/nubia/NX529J/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 

57b42e61400caa236f4e41a9abcb92bdc352334b
opticyclic <opticyclic@gmail.com>
2017-02-03 01:44:48 -0500

    Move everything from the full_NX529J.mk to the omni_NX529J

--- omni_NX529J.mk
+++ omni_NX529J.mk
@@ -18,5 +18,13 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# Release name
+PRODUCT_RELEASE_NAME := NX529J
+
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_NX529J
 PRODUCT_DEVICE := NX529J
+PRODUCT_BRAND := nubia
+PRODUCT_MODEL := NX529J
+PRODUCT_MANUFACTURER := nubia
